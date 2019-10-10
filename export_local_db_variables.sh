bde55bf8b52df209c44cb4ffd0112ec3e1265798
Johannes Elmnäinen <sanhologin@gmail.com>
2019-10-10 19:05:05 +0300

    Add DB setup instructions

---
+++ export_local_db_variables.sh
@@ -0,0 +1,6 @@
+export POSTGRES_USER='postgres'
+export POSTGRES_PASSWORD='password'
+export POSTGRES_HOST="localhost"
+export POSTGRES_PORT='5432'
+export POSTGRES_DB_NAME='annotator'
+export ENVIRONMENT='local-full-docker'
